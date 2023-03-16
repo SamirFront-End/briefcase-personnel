af6ec32f8ab47fe73a9a6acd0054add24c896688
SamirFront-End <miramirez1987@gmail.com>
2023-03-16 01:11:53 -0500

    trabajando js

--- js/function.js
+++ js/function.js
@@ -1,3 +1,7 @@
-hamburger.onclick = () => {
-    links.classList.toggle("open")
-}
+const buttonHamburger = document.getElementById("hamburger");
+const navBar = document.getElementById("barnav")
+
+navBar.onclick = () => {
+    buttonHamburger.classList.toggle("open");
+    navBar.classList.toggle("close") 
+};
