da8acb820450befdbc0e98ccaee41cc562fd43fa
SamirFront-End <miramirez1987@gmail.com>
2023-03-16 17:09:06 -0500

    terminando efectos nav

--- js/function.js
+++ js/function.js
@@ -1,1 +1,15 @@
-const buttonHamburger = document.getElementsByClassName ("navbar");
+function loadDom () {
+const menuOpen = document.getElementById ("links");
+const buttonHamburger = document.getElementById ("hamburger");
+buttonHamburger.addEventListener("click", () => {
+    buttonHamburger.classList.toggle("close");
+    menuOpen.classList.toggle("open");
+});
+menuOpen.addEventListener("click", e => {
+    if (e.target.id === "links") {
+        menuOpen.classList.toggle("open");
+        buttonHamburger.classList.remove("close");
+    }
+});
+}
+window.addEventListener("load", loadDom)
