143b46a1a49aa516d6c7e1edc08f85f97fe0a34a
SamirFront-End <miramirez1987@gmail.com>
2023-03-16 11:57:01 -0500

    mejorando js

--- js/function.js
+++ js/function.js
@@ -1,7 +1,1 @@
-const buttonHamburger = document.getElementById("hamburger");
-const navBar = document.getElementById("barnav")
-
-navBar.onclick = () => {
-    buttonHamburger.classList.toggle("open");
-    navBar.classList.toggle("close") 
-};
+const buttonHamburger = document.getElementsByClassName ("navbar");
