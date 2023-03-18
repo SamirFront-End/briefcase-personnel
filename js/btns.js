9297d8376480209d7c18f8240f5efcdefcbcf7b4
SamirFront-End <miramirez1987@gmail.com>
2023-03-18 15:42:41 -0500

    prueba5 en server

--- js/btns.js
+++ js/btns.js
@@ -6,5 +6,5 @@ function repositorio(){
     window.location.href="../repoout.html";
 }
 function demo(){
-    window.location.href="../demoout.html"
+    window.location.href="https://samirfront-end.github.io/demoout.html"
 }
